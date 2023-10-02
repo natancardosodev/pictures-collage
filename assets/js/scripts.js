25b9e321849104569daf396e219b6ecdd5b1e1fe
Natan Cardoso <natan@voxtecnologia.com.br>
2023-10-01 23:19:23 -0300

    melhorias e add read json

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,25 +13,44 @@ $(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const eventName = urlParams.get('evento');
     predefinedImage.src = `./assets/images/${eventName}.png`;
-    $('#nameEvent').html(`#${eventName}`);
-
-    //fetch(`/assets/configs/${eventName}.json`)
-    //.then(function (response) {
-    //    response = dadosEvento;
-    // });
-
-    $('#btnCreditos').on('click', () => {
-        $('#creditosDesc').toggle(300);
-    });
 
     predefinedImage.onload = function () {
         $('#resultImage').html(predefinedImage);
+
+        if (localStorage.getItem(eventName) && localStorage.getItem(eventName).length) {
+            dadosEvento = JSON.parse(atob(localStorage.getItem(eventName)));
+            setDadosEvento();
+
+            return;
+        }
+
+        fetch(`/assets/configs/${eventName}.json`)
+            .then((response) => response.json())
+            .then((response) => {
+                dadosEvento = response;
+                localStorage.setItem(eventName, btoa(JSON.stringify(dadosEvento)));
+                if (dadosEvento) {
+                    setDadosEvento();
+                }
+            });
     };
 
     predefinedImage.onerror = () => {
         $('#wrapper').hide();
         $('#msg-error').show();
     };
+
+    function setDadosEvento() {
+        predefinedImage.alt = dadosEvento.data.title;
+        predefinedImage.width = dadosEvento.sizes.width;
+        predefinedImage.height = dadosEvento.sizes.height;
+        $('#border-secondary').css('background-color', dadosEvento.colors.secondary);
+        $('#content-evento').css('background-color', dadosEvento.colors.primary);
+        $('#title').html(dadosEvento.data.title);
+        $('#date').html(dadosEvento.data.date);
+        $('#link-evento').attr('href', dadosEvento.data.link).css('color', dadosEvento.colors.text);
+        $('h4').css('color', dadosEvento.colors.primary);
+    }
 
     // Manipule o upload da imagem
     $('#uploadImage').on('change', (e) => {
@@ -53,7 +72,7 @@ $(() => {
         if (file) {
             const reader = new FileReader();
             reader.onload = function (e) {
-                const uploadedImageElement = new Image(1080, 1170);
+                const uploadedImageElement = new Image(dadosEvento.sizes.width, dadosEvento.sizes.height);
                 uploadedImageElement.src = e.target.result;
                 uploadedImageElement.alt = 'Modelo de Cartaz #EuVou';
                 uploadedImage = uploadedImageElement;
@@ -61,53 +80,61 @@ $(() => {
                 buttonLink.hide();
                 buttonCortar.show();
 
-                setTimeout(() => {
-                    if (uploadedImage) {
-                        // Crie um elemento de tela de canvas para mesclar as imagens
-                        const canvas = document.createElement('canvas');
-                        canvas.width = predefinedImage.width;
-                        canvas.height = predefinedImage.height;
-                        const ctx = canvas.getContext('2d');
+                if (uploadedImage) {
+                    // Crie um elemento de tela de canvas para mesclar as imagens
+                    const canvas = document.createElement('canvas');
+                    canvas.width = predefinedImage.width;
+                    canvas.height = predefinedImage.height;
+                    const ctx = canvas.getContext('2d');
 
-                        var cropper = new Cropper(uploadedImage, {
-                            aspectRatio: 1 / 1.1083, // 1170 / 1080
-                            viewMode: 3,
-                            dragMode: 'move',
-                            center: true,
-                            minCropBoxWidth: 650,
-                            minCropBoxHeight: 150,
-                            cropBoxMovable: true,
-                            cropBoxResizable: true,
-                            ready: function (event) {
-                                // cropper.zoomTo(1);
-                            },
+                    var cropper = new Cropper(uploadedImage, {
+                        aspectRatio: 1 / (dadosEvento.sizes.height / dadosEvento.sizes.width),
+                        viewMode: 3,
+                        dragMode: 'move',
+                        center: true,
+                        minCropBoxWidth: 650,
+                        minCropBoxHeight: 150,
+                        cropBoxMovable: true,
+                        cropBoxResizable: true,
+                        ready: function (event) {
+                            // cropper.zoomTo(1);
+                        },
 
-                            crop: function (event) {
-                                cropper.scale(1, 1);
-                            },
+                        crop: function (event) {
+                            cropper.scale(1, 1);
+                        },
 
-                            zoom: function (event) {
-                                if (event.detail.oldRatio === 1) {
-                                    event.preventDefault();
-                                }
+                        zoom: function (event) {
+                            if (event.detail.oldRatio === 1) {
+                                event.preventDefault();
                             }
-                        });
+                        }
+                    });
 
-                        buttonCortar.on('click', (e) => {
-                            result.innerHTML = '';
-                            ctx.drawImage(cropper.getCroppedCanvas(), 0, 0, 1080, 1170);
-                            ctx.drawImage(predefinedImage, 0, 0, 1080, 1170);
+                    buttonCortar.on('click', (e) => {
+                        result.innerHTML = '';
+                        ctx.drawImage(
+                            cropper.getCroppedCanvas(),
+                            0,
+                            0,
+                            dadosEvento.sizes.width,
+                            dadosEvento.sizes.height
+                        );
+                        ctx.drawImage(predefinedImage, 0, 0, dadosEvento.sizes.width, dadosEvento.sizes.height);
 
-                            const mergedImage = new Image();
-                            mergedImage.src = canvas.toDataURL('image/png');
-                            result.html(mergedImage);
-                            buttonCortar.hide();
-                            buttonLink.attr('href', mergedImage.src).show();
-                        });
-                    }
-                }, 50);
+                        const mergedImage = new Image();
+                        mergedImage.src = canvas.toDataURL('image/png');
+                        result.html(mergedImage);
+                        buttonCortar.hide();
+                        buttonLink.attr('href', mergedImage.src).show();
+                    });
+                }
             };
             reader.readAsDataURL(file);
         }
+    });
+
+    $('#btnCreditos').on('click', () => {
+        $('#creditosDesc').toggle(300);
     });
 });
